1058cbee57457937114e19faf124dcb146a87ee1
skolens <skolens@example.com>
2024-10-18 12:16:44 +0300

    mm

--- uzd1.py
+++ uzd1.py
@@ -7,4 +7,5 @@ if vecums < 13 :
 elif vecums >= 20:
   print("tu esi pieaugušais")#ja tavs vecums ir lielāks vai vienāds par 20 gadiem, tad tu esi pieaugušais
 elif vecums >= 13 and vecums <= 19:
-  print("tu esi tīnis")
+  print("tu esi tīnis")#ja tavs vecums ir vienāds vai lielāks par 13 gadiem un vienāds vai mazāks par 20 gadiem, tad tu esi tīnis
+  
---
+++ uzd2.py
@@ -0,0 +1,12 @@
+#RafaelsDambis18.10.2024
+correct_password = "python123"#Nosaka pareizo paroli
+
+while True:#sāk ciklu
+  parole = input("Ieraksti paroli")#tev paprasa paroli
+  if parole == correct_password:
+    print("Piekļuve atļauta")#ja patole ir pareiza, tad piekļuve ir atļauta
+    
+  else:
+    print("Piekļuve liegta")#ja patole ir nepareiza, tad piekļuve ir liegta
+
+    
