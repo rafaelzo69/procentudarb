817f77ec867ac492f157a2012ec97f6796fdde1f
skolens <skolens@example.com>
2024-10-18 11:50:59 +0300

    first commit

---
+++ uzd1.py
@@ -0,0 +1,10 @@
+#RafaelsDambis18.10.2024
+vecums = int(input("Kāds ir tavs vecums"))#terminālis tev jautās tavu vecumu
+
+
+if vecums < 13 :
+  print("tu esi bērns")#ja tavs vecums ir mazāks par 13 gadiem, tad tu esi bērns
+elif vecums >= 20:
+  print("tu esi pieaugušais")#ja tavs vecums ir lielāks vai vienāds par 20 gadiem, tad tu esi pieaugušais
+elif vecums >= 13 and vecums <= 19:
+  print("tu esi tīnis")
